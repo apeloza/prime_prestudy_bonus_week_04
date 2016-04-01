5e2c50e1396db2094e85b03902ff955173eac5b0
Tony Peloza <tonypeloza@Roms-MacBook-Air.local>
2016-04-01 15:10:23 -0500

    Bonus challenge edits

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hey this works!");
+function plusTwo(num){
+	return num+2;
+}
+function moduloThree(num){
+	return num%3;
+}
+function timesTwo(num){
+	return num*2;
+}
+
+console.log("4 modulo 3 times 2 plus 2 = " + plusTwo(timesTwo(moduloThree(4))));
